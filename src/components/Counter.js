5a51f3279fb545aaaee347c65fc7b9514dfa0108
tester <tester@cheoeum.in>
2017-06-07 18:34:30 +0900

    to do at home

---
+++ src/components/Counter.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Counter.css'
+
+const Counter = ({number, color, onSetColor, onIncrease, onDecrease}) => (
+	<div className="Counter"
+	     onClick={onIncrease}
+	     onContextMenu={
+		     (e) => {
+		     	  e.preventDefault()
+			     onDecrease()
+		     }
+	     }
+	     onDoubleClick={onSetColor}
+	     style={
+		     {
+		     	backgroundColor: color
+		     }
+	     }
+	>
+		{number}
+	</div>
+)
+
+Counter.propTypes = {
+	number: PropTypes.number,
+	color:  PropTypes.string,
+	onSetColor: PropTypes.func,
+	onIncrease: PropTypes.func,
+	onDecrease: PropTypes.func
+}
+
+Counter.defaultProps = {
+	number: 0,
+	color: '#000',
+	onSetColor: () => console.log('set color'),
+	onIncrease: () => console.log(this.props),
+	onDecrease: () => console.log('decrease')
+}
+
+export default Counter
